97dd400ace9e9b79cc373b7b43514ff0940c03ce
takanamito <takahiro.kiso@zozo.com>
2020-09-12 13:30:14 +0900

    mysql

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ export PATH=/home/isucon/local/ruby/bin:$PATH
 sudo cp ./isuumo.ruby.service /etc/systemd/system/isuumo.ruby.service
 sudo cp ./nginx.conf /etc/nginx/nginx.conf
 sudo cp ./isuumo.conf /etc/nginx/sites-available/isuumo.conf
+sudo cp ./mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
 sudo cp ./sysctl.conf /etc/sysctl.conf
 
 cd /home/isucon/isuumo/webapp/ruby
@@ -13,4 +15,6 @@ bundle check || bundle install --path ./vender/bundle --jobs 300
 
 sudo systemctl daemon-reload
 sudo sysctl -p
+sudo systemctl restart mysql
 sudo systemctl restart isuumo.ruby
+sudo systemctl restart nginx
