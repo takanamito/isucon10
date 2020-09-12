b5adcef9323330f1785fadcf8fafb1aa931c6c12
takanamito <takahiro.kiso@zozo.com>
2020-09-12 13:25:07 +0900

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 export PATH=/home/isucon/local/ruby/bin:$PATH
 
-cd /home/isucon/isuumo/webapp/ruby
-git pull origin master
-bundle check || bundle install --path ./vender/bundle --jobs 300
-
 sudo cp ./isuumo.ruby.service /etc/systemd/system/isuumo.ruby.service
 sudo cp ./nginx.conf /etc/nginx/nginx.conf
 sudo cp ./isuumo.conf /etc/nginx/sites-available/isuumo.conf
 sudo cp ./sysctl.conf /etc/sysctl.conf
+
+cd /home/isucon/isuumo/webapp/ruby
+git pull origin master
+bundle check || bundle install --path ./vender/bundle --jobs 300
 
 sudo systemctl daemon-reload
 sudo sysctl -p
