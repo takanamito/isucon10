485b2a6c04417828a566d7a0ef192930485382e4
Yu Shigetani <s2g4t1n2@gmail.com>
2020-09-12 17:14:42 +0900

    Revert "buy speed up"
    This reverts commit 018e058c34c58028b44e0f1be4d9e0727dcf3db2.

--- ruby/app.rb
+++ ruby/app.rb
@@ -303,13 +303,12 @@ class App < Sinatra::Base
       end
 
     transaction('post_api_chair_buy') do |tx_name|
-      chair = db.xquery('SELECT * FROM chair WHERE id = ? FOR UPDATE', id).first
-      if !chair || chair[:stock] == 0
+      chair = db.xquery('SELECT * FROM chair WHERE id = ? AND stock > 0 FOR UPDATE', id).first
+      unless chair
         rollback_transaction(tx_name) if in_transaction?(tx_name)
         halt 404
       end
-      stock = chair[:stock] - 1
-      db.xquery('UPDATE chair SET stock = ? WHERE id = ?', stock, id)
+      db.xquery('UPDATE chair SET stock = stock - 1 WHERE id = ?', id)
     end
 
     status 200
