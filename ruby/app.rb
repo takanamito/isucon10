b853fb640e8fa91cc3a16122f63372d83d966366
Yu Shigetani <s2g4t1n2@gmail.com>
2020-09-12 17:54:37 +0900

    revert

--- ruby/app.rb
+++ ruby/app.rb
@@ -451,20 +451,30 @@ class App < Sinatra::Base
     estates = db.xquery(sql, bounding_box[:bottom_right][:latitude], bounding_box[:top_left][:latitude], bounding_box[:bottom_right][:longitude], bounding_box[:top_left][:longitude])
 
     estates_in_polygon = []
-    conds = []
-    ids = []
+    # conds = []
+    # ids = []
+    #
+    # estates.each do |estate|
+    #   coordinates_to_text = "'POLYGON((%s))'" % coordinates.map { |c| '%f %f' % c.values_at(:latitude, :longitude) }.join(',')
+    #   point = "'POINT(%f %f)'" % estate.values_at(:latitude, :longitude)
+    #   conds << "(id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s)))" % [coordinates_to_text, point]
+    #   ids << estate[:id]
+    # end
+    #
+    # unless conds.empty?
+    #   cond = conds.join(' OR ')
+    #   sql = "SELECT * FROM estate WHERE #{cond}"
+    #   estates_in_polygon = db.xquery(sql, *ids).to_a.compact
+    # end
 
     estates.each do |estate|
-      coordinates_to_text = "'POLYGON((%s))'" % coordinates.map { |c| '%f %f' % c.values_at(:latitude, :longitude) }.join(',')
-      point = "'POINT(%f %f)'" % estate.values_at(:latitude, :longitude)
-      conds << "(id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s)))" % [coordinates_to_text, point]
-      ids << estate[:id]
-    end
-
-    unless conds.empty?
-      cond = conds.join(' OR ')
-      sql = "SELECT * FROM estate WHERE #{cond}"
-      estates_in_polygon = db.xquery(sql, *ids).to_a.compact
+     point = "'POINT(%f %f)'" % estate.values_at(:latitude, :longitude)
+     coordinates_to_text = "'POLYGON((%s))'" % coordinates.map { |c| '%f %f' % c.values_at(:latitude, :longitude) }.join(',')
+     sql = 'SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))' % [coordinates_to_text, point]
+     e = db.xquery(sql, estate[:id]).first
+     if e
+       estates_in_polygon << e
+     end
     end
 
     nazotte_estates = estates_in_polygon.take(NAZOTTE_LIMIT)
